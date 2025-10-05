1644302dd8648ec148b7ffd13d82949dd6964431
wiliam-bermudez <wbermdezsols@byupathway.edu>
2025-10-05 15:18:30 -0600

    Ordenado a la mitad

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,30 @@
+export async function loadDiscover() {
+  const container = document.getElementById("discover-container");
+  if (!container) return;
+
+  try {
+    const response = await fetch("data/discover.json");
+    const data = await response.json();
+
+    container.innerHTML = "";
+    data.forEach(business => {
+      const card = document.createElement("div");
+      card.classList.add("business-card");
+
+      card.innerHTML = `
+          <figure>
+          <img src="${business.image_url}" alt="${business.name} logo" loading="lazy">
+          </figure>
+          <h2>${business.name}</h2>
+          <address>${business.address}</address>
+          <p>${business.description}</p>
+          <button class="learn-more">Learn More</button>
+      `;
+      container.appendChild(card);
+    });
+
+  } catch (error) {
+    console.error("Error loading businesses:", error);
+    container.innerHTML = "<p>Unable to load business data.</p>";
+  }
+}
--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -3,6 +3,8 @@ import { setupMenu } from "./menu.js";
 import { loadWeather } from "./weather.js";
 import { loadSpotlights } from "./chamberHome.js";
 import { loadDirectory } from "./chamber.js";
+import { loadDiscover } from "./discover.js";
+
 
 setupMenu();
 
@@ -16,4 +18,8 @@ if (isPage("index.html")) {
 
 if (isPage("directory.html")) {
   loadDirectory();
+}
+
+if (isPage("discover.html")) {
+  loadDiscover();
 }
