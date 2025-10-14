27a4025e6af1cefdd7f99caa8103db998a62ba45
wiliam-bermudez <wbermdezsols@byupathway.edu>
2025-10-14 17:48:11 -0600

    Menu Done

--- finalproject/scripts/index.js
+++ finalproject/scripts/index.js
@@ -85,6 +85,10 @@ function setupMenu() {
 
 async function loadMenu() {
   const container = document.getElementById("menu-container");
+  const Modal = document.getElementById("Desc-modal");
+  const Closemodal = document.getElementById("closeModal");
+  const contentModal = document.getElementById("Content-modal");
+
   if (!container) return;
 
   try {
@@ -94,24 +98,48 @@ async function loadMenu() {
 
     container.innerHTML = "";
     dishes.forEach(dish => {
-      const card = document.createElement("div");
-      card.classList.add("menu-card-dis");
+  const card = document.createElement("div");
+  card.classList.add("menu-card-dis");
 
-      card.innerHTML = `
-          <figure>
-          <img src="${dish.photo}" alt="${dish.name} logo" loading="lazy">
-          </figure>
-          <h2>${dish.name}</h2>
-          <p>${dish.description}</p>
-          <button class="learn-more">Learn More</button>
-      `;
+  card.innerHTML = `
+      <figure>
+        <img src="${dish.photo}" alt="${dish.name} logo" loading="lazy">
+      </figure>
+      <h2>${dish.name}</h2>
+      <p>${dish.price}</p>
+      <button class="learn-more-btn" data-name="${dish.name}" data-description="${dish.description}" data-price="${dish.price}">Learn More</button>
+  `;
       container.appendChild(card);
     });
+
+   const modalButtons = document.querySelectorAll(".learn-more-btn");
+
+     modalButtons.forEach(btn => {
+       btn.addEventListener("click", () => {
+         const name = btn.getAttribute("data-name");
+         const description = btn.getAttribute("data-description");
+         const price = btn.getAttribute("data-price");
+
+         contentModal.innerHTML = `
+           <h2>${name}</h2>
+           <p>${description}</p>
+           <strong>Price: ${price}</strong>
+         `;
+
+         Modal.showModal();
+       });
+     });
+
+     Closemodal.addEventListener("click", () => {
+       Modal.close();
+     });
 
   } catch (error) {
     console.error("Error loading businesses:", error);
     container.innerHTML = "<p>Unable to load business data.</p>";
   }
 }
+
+
 
 loadMenu()
