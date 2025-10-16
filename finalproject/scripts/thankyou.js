93fae93028f125c1c6148ca53325dd607caf00ab
wiliam-bermudez <wbermdezsols@byupathway.edu>
2025-10-16 15:49:08 -0600

    Noe yes

--- finalproject/scripts/thankyou.js
+++ finalproject/scripts/thankyou.js
@@ -1,22 +1,15 @@
-export function setupThankYou() {
-  window.addEventListener("DOMContentLoaded", () => {
-    window.addEventListener("DOMContentLoaded", () => {
-      const params = new URLSearchParams(window.location.search);
-      console.log(params.get("fullname"));
-      console.log(params.get("phonenumber"));
-      console.log(params.get("dateEvent"));
-      console.log(params.get("email"));
-      console.log(params.get("phone"));
-      console.log(params.get("eventType"));
+// thankyou.js
+window.addEventListener("DOMContentLoaded", () => {
+  const params = new URLSearchParams(window.location.search);
 
+  const results = document.getElementById('formInfo');
+  if (!results) return;
 
-    const results = document.getElementById('formInfo');
-    results.innerHTML = `
+  results.innerHTML = `
     <p><strong>First name:</strong> ${params.get('fullname')}.</p>
     <p><strong>Email:</strong> ${params.get('email')}.</p>
     <p><strong>Phone:</strong> ${params.get('phonenumber')}.</p>
     <p><strong>Date of Event:</strong> ${params.get('dateEvent')}.</p>
-    <p><strong>Type of Event:</strong> ${params.get('eventType')}.</p>`
-    });
-  });
-}
+    <p><strong>Type of Event:</strong> ${params.get('eventType')}.</p>
+  `;
+});
